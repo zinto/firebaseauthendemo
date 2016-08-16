057550b77e2ecafe1471a45e9bf6965eaae93fdd
zinto <zinhumgato@gmail.com>
2016-08-16 23:25:24 +0700

    first commit

---
+++ app/src/main/java/com/anhtuan89/firebaseauthendemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anhtuan89.firebaseauthendemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
